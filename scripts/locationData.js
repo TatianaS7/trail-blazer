101654803be0c8faba743d1c71bdc6d705c79bed
Tatiana <tstwrt7@gmail.com>
2023-05-23 18:39:36 -0400

    ""

---
+++ scripts/locationData.js
@@ -0,0 +1,75 @@
+const locationsArray = [
+    "Location..",
+    "Alabama",
+    "Alaska",
+    "American Samoa",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "DC",
+    "Florida",
+    "Georgia",
+    "Guam",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Puerto Rico",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Vermont",
+    "Virgin Islands",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+    "Wyoming"
+]
+
+//By Location 
+
+function populateLocationDropdown() {
+    const selectElement = document.querySelector("#by-location");
+
+    for (const location of locationsArray) {
+
+        const option = document.createElement("option");
+
+        option.value = location
+        option.innerText = location
+
+        selectElement.append(option)
+    }
+}
+
